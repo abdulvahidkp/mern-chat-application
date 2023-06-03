036012d1e6896d3e5cc0949e7a4c3dc8b1fc4019
abdulvahidkp <abdulvahid1117@gmail.com>
2023-06-03 21:41:02 +0530

    schema created at server side

---
+++ server/MODELS/chats.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const chatSchema = new mongoose.Schema({
+    chatName:{
+        type:String,
+        trim:true
+    },
+    isGroupChat:{
+        type:Boolean,
+        default:false
+    },
+    users:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"users"
+    }],
+    latestMessage:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"messages"
+    }],
+    groupAdmin:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"users"
+    }
+},{
+    timestamps:true
+});
+
+module.exports = mongoose.model('chats',chatSchema);
---
+++ server/MODELS/messages.modal.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+    sender:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'users'
+    },
+    content:{
+        type:String,
+        trim:true
+    },
+    chat: {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'chats'
+    }
+},{
+    timestamps:true
+});
+
+module.exports = mongoose.model('messages',messageSchema)
