61803bc133b94df2e49dfb022808943e2e2cfc89
abdulvahidkp <abdulvahid1117@gmail.com>
2023-06-04 11:49:37 +0530

    login page and signup page ui maded

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,17 +4,18 @@ import './App.css'
 import {Routes,Route} from 'react-router-dom'
 import Homepage from './pages/Homepage'
 import Chatpage from './pages/Chatpage'
+import './App.css'
 
 function App() {
 
   return (
-    <>
+    <div className='App'>
       <Routes>
         <Route path='/' element={<Homepage/>}/>
         <Route path='/chat' element={<Chatpage/>}/>
 
       </Routes>
-    </>
+    </div>
   )
 }
 
